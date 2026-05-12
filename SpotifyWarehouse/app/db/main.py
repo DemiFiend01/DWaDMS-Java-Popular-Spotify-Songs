7ca48ae0356075f952f5acd0d8ad4bdae2a2b3bd
FitzPatrick2002 <miloszlini@gmail.com>
2026-05-12 13:33:01 +0200

    Working on loading data into statging area.

--- SpotifyWarehouse/app/db/main.py
+++ SpotifyWarehouse/app/db/main.py
@@ -14,9 +14,10 @@ from matplotlib import pyplot as plt
 #    - (ok) Null values -> None found
 #    - (ok) Normalize some of them
 #    - (ok) Min & max for ranges?
-# 5. Repeating values -> covers
-#    - Make unique -> add column ['Title + artist'] -> append ' by {Artist}' at the end
+# 5. (nope) Repeating values -> covers
+#    - (nope) Make unique -> add column ['Title + artist'] -> append ' by {Artist}' at the end
 # 6. Insert data into intermediate table?
+#    - Read about psycopg interaction with.csv
 # 7. Insert into the DB
 
 scripts = Path("scripts/")
@@ -44,22 +45,27 @@ def load_data_to_staging_area(src : str | Path):
 
     df = pd.read_csv(src)
 
+    # 1. Construct the SQL query
+
+    cols_num = len(df.columns)
+    query = "(" + "%s," * (cols_num - 1) + "%s)"
+    query += 
+
     with get_conn() as conn:
         with conn.cursor() as cur:
-            pass
+            for idx, row in df.iterrows():
+                cur.execute("INSERT INTO full_dataset "
+                            "()"
+                            "VALUES ()")
 
-
-def clean_dataset():
-    # 0. Read csv
+def explore_dataset():
 
     df = pd.read_csv(datasets / "spotify-dataset.csv")
-
-    # 0.1 Explore df
 
     print(f"Column types: {df.dtypes}")
     print(f"Shape: {df.shape}")
     print(f"Title & views: {df.loc[[0, 5], ['Track', 'Views']]}")
-    print(f"Title - Views: {df.loc[0 : 5, 'Track' : 'Views']}")
+    print(f"Title - Views: {df.loc[0: 5, 'Track': 'Views']}")
     print(f"[5, 1]: {df.iloc[5, 1]}")
     print(f"Index: {df.index}")
     print(f"Columns: {df.columns}")
@@ -77,53 +83,59 @@ def clean_dataset():
         print(f"{col} :")
         print(f"{df[col].value_counts()}")
 
+def clean_dataset(normalize=False):
+    '''
+    Removes null values from spotify-dataset.csv & rows which have not information about some youtube statistics.
+    Normalization of some metrics is commented out.
+    '''
+
+    # 0. Read csv
+
+    df = pd.read_csv(datasets / "spotify-dataset.csv")
+
     # 1. Null values removed
 
     # Null values: ['Channel'] & ['Title'] == 0
     not_on_yt = (df["Channel"] == '0') & (df["Title"] == '0')
     cleaned_df = df[~not_on_yt]
-    print("Not on yt: ")
-    print(df[not_on_yt])
-
-    print("Cleaned:")
-    print(cleaned_df)
 
     # 2. Find min / max for each range column
 
     range_cols = ["Danceability", "Energy", "Loudness", "Speechiness", "Acousticness", "Instrumentalness", "Liveness", "Valence", "Tempo", "Duration_min", "Views", "Likes", "Comments", "Stream", "EnergyLiveness"]
     ranges_df = df.loc[ : , range_cols]
-    print("Ranges df:")
-    print(ranges_df)
-
     maxes = ranges_df.max()
     mins = ranges_df.min()
-
-    print(f"Maxes: {maxes}")
-    print(f"Mins: {mins}")
 
     min_maxes = list(zip(mins, maxes))
     ranges = dict(zip(range_cols, min_maxes))
 
-    print(f"Ranges: {ranges}")
+    # Save ranges into separate df
 
-    # 3. Normalize the cleaned df
+    ranges_df = pd.DataFrame(ranges)
+    ranges_df.to_csv(datasets / "spotify-dataset-ranges.csv")
 
-    clean_norm_df = (ranges_df - ranges_df.min()) / (ranges_df.max() - ranges_df.min())
+    if normalize:
+        # 3. Normalize the cleaned df
+        clean_norm_df = (ranges_df - ranges_df.min()) / (ranges_df.max() - ranges_df.min())
 
-    # 4. Replace cols in cleaned df with normalized ones
-
-    for col in clean_norm_df.columns:
-        new_col = col + "-norm"
-        cleaned_df[new_col] = clean_norm_df[col]
-
-    print("Cleaned & Normalized df:")
-    print(cleaned_df)
+        # 4. Replace cols in cleaned df with normalized ones
+        for col in clean_norm_df.columns:
+            new_col = col + "-norm"
+            cleaned_df[new_col] = clean_norm_df[col]
 
     # 5. Save to 'spotify-dataset-clean.csv'
-
     cleaned_df.to_csv(datasets / "spotify-dataset-clean.csv")
 
+def dataset_messing():
+    df = pd.read_csv(datasets / "spotify-dataset-clean.csv")
 
+    _, row = next(df.iterrows())
+    cols = [col.lower() for col in tuple(row.index)]
+    vals = row.values
+    print(cols)
+    print(vals)
 
+# dataset_messing()
+clean_dataset()
 
 
