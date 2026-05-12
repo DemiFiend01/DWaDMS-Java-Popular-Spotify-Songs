c8a9f1c95e99dfc78c00913f6f6a8a6101361bcf
DemiFiend01 <maggie.mr1606@gmail.com>
2026-05-12 15:18:15 +0200

    protection when no pictures present

--- SpotifyWarehouse/app/flask/view.py
+++ SpotifyWarehouse/app/flask/view.py
@@ -3,24 +3,30 @@ from os import listdir
 from os.path import isfile, join
 
 class View:
-    def __init__(self):
+    def __init__(self, output_path: str = ""):
         self.app = Flask(__name__)
         self.app.add_url_rule("/", view_func=self.start)
         self.pictures=[]
+        self.output_path = output_path
 
     def start(self):
+        self.load_pictures(self.output_path)
+        if(self.pictures.count==0):
+            return render_template('index.html')
         return render_template('index.html', pictures=self.pictures)
   
     def load_pictures(self, path: str):
-        fs_path = "app/flask" + path
-        self.pictures = [
-            {"src" : path + "/" + p }
-            for p in listdir(fs_path) if isfile(join(fs_path,p)) and p.endswith(".png")
-        ]
+        try:
+            fs_path = "app/flask" + path
+            self.pictures = [
+                {"src" : path + "/" + p }
+                for p in listdir(fs_path) if isfile(join(fs_path,p)) and p.endswith(".png")
+            ]
+        except:
+            print("Failed to load pictures")
 
-test = View()
+test = View(output_path="/static/output")
 
 if __name__ == "__main__":
-    test.load_pictures(path="/static/output")
     test.app.run(debug="true")
     
