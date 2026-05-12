27c4fd2b1fe1d2d6c9517ac7cfa646e2fa186a65
DemiFiend01 <maggie.mr1606@gmail.com>
2026-05-12 15:13:01 +0200

    frontend done

--- SpotifyWarehouse/app/flask/view.py
+++ SpotifyWarehouse/app/flask/view.py
@@ -1,27 +1,26 @@
-from flask import Flask
-#from pathlib import Path #never do this path, it does not work
-#put the graphs in static directory
+from flask import Flask, render_template
+from os import listdir
+from os.path import isfile, join
 
 class View:
     def __init__(self):
-        self.app = Flask(__name__, static_folder="../static", static_url_path="/static")
+        self.app = Flask(__name__)
         self.app.add_url_rule("/", view_func=self.start)
-        self.site_code = "<h1>Spotify Datawarehouse Project</h1>"
-        self.image_id = 1
+        self.pictures=[]
 
     def start(self):
-        return self.site_code
-    
-    def add_picture(self, path, desc: str):
-        self.site_code += "<img id=\"picture" + str(self.image_id) +"\" src=" + str(path) + " height=500 ></br>"
-        self.site_code += "<label for=\"picture" + str(self.image_id) +"\" >" + desc + "</label></br>"
-        self.image_id += 1
-
+        return render_template('index.html', pictures=self.pictures)
+  
+    def load_pictures(self, path: str):
+        fs_path = "app/flask" + path
+        self.pictures = [
+            {"src" : path + "/" + p }
+            for p in listdir(fs_path) if isfile(join(fs_path,p)) and p.endswith(".png")
+        ]
 
 test = View()
 
 if __name__ == "__main__":
-    test.add_picture(path="/static/output/earlybird.png", desc="Early bird")
-    test.add_picture(path="/static/output/night_owl.png", desc="Night Owl")
+    test.load_pictures(path="/static/output")
     test.app.run(debug="true")
     
